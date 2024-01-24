ed939ca009a9a363f3b67f083538ac2427265f22
Jobie Kirkwood <jobie.kirkwood@york.ac.uk>
2024-01-24 23:56:34 +0000

    Removed DAL

--- DataAccess/Models/TaxCategory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DataAccess.Models
-{
-    public class TaxCategory
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public double Rate { get; set; }
-    }
-}
